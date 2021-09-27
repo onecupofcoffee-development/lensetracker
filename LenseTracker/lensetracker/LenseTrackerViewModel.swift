41565bdeef763d462ac90b30b89c8d1ca54b8a22
Andey Lesnykh <galmir.irk@gmail.com>
2021-09-27 20:52:30 +0300

    notifications mvp
    implemented the mvp of notifications (daily reminder to take lenses off)

--- LenseTracker/lensetracker/LenseTrackerViewModel.swift
+++ LenseTracker/lensetracker/LenseTrackerViewModel.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import CoreData
+import UserNotifications
 
 class LenseTrackerViewModel : ObservableObject {
     @Published var myModel: LenseTrackerModel {
@@ -23,25 +24,72 @@ class LenseTrackerViewModel : ObservableObject {
         if let savedData = UserDefaults.standard.object(forKey: "LenseData") as? Data {
             if let savedStruct = try? decoder.decode(LenseTrackerModel.self, from: savedData) {
                 self.myModel = savedStruct
-                print("decoded data is \(savedStruct)")
+                //print("decoded data is \(savedStruct)")
                 return
             }
         }
         self.myModel = LenseTrackerModel()
     }
     
+    private func throwNotification(reminderTime: Int) {
+        let content = UNMutableNotificationContent()
+        //calculating reminder time
+        let calendar = Calendar.current
+        let hour = calendar.component(.hour, from: Date())
+        let minutes = calendar.component(.minute, from: Date())
+        let interval = reminderTime*60*60-(hour*60+minutes)*60
+        print("seconds left to notify: \(interval)")
+        content.title = "Вы не забыли снять линзы?"
+        content.subtitle = "Для точного учета срока годности линзы, нажмите 'Снять линзы' в приложении LenseTracker"
+        content.sound = UNNotificationSound.default
+
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: TimeInterval(interval), repeats: false)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request) { (error) in
+            if error != nil {
+               print("shit happened")
+            }
+         }
+    }
+    
+    private func setLenseReminder(time: Int) {
+        
+        let center = UNUserNotificationCenter.current()
+        
+        center.getNotificationSettings
+        { settings in
+             if settings.authorizationStatus == .notDetermined {
+                        print("notification auth is not set")
+                        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound])
+                            {
+                                success, error in
+                                    if success {
+                                        self.throwNotification(reminderTime: time)
+                                    }
+                                else if let error = error { print(error) }
+                            }
+             } else {
+                 self.throwNotification(reminderTime: time)
+             }
+        }
+    }
+    
     //MARK: Intents
     
     func PutLensesOn() {
         myModel.putOn()
+        setLenseReminder(time: 22)
     }
     
     func takeLensesOff() {
         myModel.takeOff()
+        UNUserNotificationCenter.current().removeAllDeliveredNotifications()
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
     }
 
     func createNewLenses(_ force: Double, _ valid: Int) {
         myModel.createNew(force: force, valid: valid)
+        setLenseReminder(time: 22)
     }
     
     func getMyLensesState() -> Bool {
